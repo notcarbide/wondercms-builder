7dfedeb872d4fba844e2980e158c174e8cfbf8d8
Filthy Luis <notcarbide@users.noreply.github.com>
2019-02-27 16:01:53 -0600

    sh: build: Changes to allow local building
    Also:
    - Print zip location on successful build
    - Organize by branch & date

--- build.sh
+++ build.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 # Build a zip archive with wondercms files
 
+CDATE=`date "+%Y-%m-%d"`
 # change this to master once the dev branch has been merged
 BRANCH=dev
 # change this to the proper location of the zip file
-ZIP_PATH=/tmp/latest.zip
+ZIP_PATH=`pwd`/out
+ZIP_FILE=$ZIP_PATH/wondercms_$BRANCH-$CDATE.zip
 
-TMP_DIR=/tmp/wondercms-$RANDOM
+TMP_DIR=tmp/wondercms_$BRANCH-$CDATE
+
+# attempt to make tmp directory if does not exist
+if [ ! -d "$TMP_DIR" ]; then
+    mkdir -p "$TMP_DIR"
+fi
 
 # get the latest files
 git clone --depth 1 -b "$BRANCH" https://github.com/robiso/wondercms "$TMP_DIR"
@@ -21,8 +28,17 @@ composer install
 
 # make zip archive
 # but first remove the previous one (or it will just append to it)
-if [ -f "$ZIP_PATH" ]; then
-    rm "$ZIP_PATH"
+if [ -f "$ZIP_FILE" ]; then
+    rm "$ZIP_FILE"
+fi
+# also attempt to make zip path if does not exist
+if [ ! -d "$ZIP_PATH" ]; then
+    mkdir -p "$ZIP_PATH"
 fi
 
-zip -r "$ZIP_PATH" assets index.php plugins src/classes themes vendor version
+zip -r "$ZIP_FILE" assets index.php plugins src/classes themes vendor version
+
+if [ -f "$ZIP_FILE" ]; then
+    echo "Build in $TMP_DIR successful!"
+    echo "Output: $ZIP_FILE"
+fi
